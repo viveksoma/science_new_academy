16fbfab9d97359d2cbb79f6ba7f2c3a4758f9513
Vivek Somasundaram <viveksomasundaram@Viveks-MacBook-Air.local>
2025-06-27 21:49:32 +0530

    Added all pages

---
+++ send_mail.php
@@ -0,0 +1,29 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $to = 'no-reply@sciencenewacademy.in';
+    $subject = 'New Contact Message from Website';
+
+    $name = htmlspecialchars($_POST['name'] ?? '');
+    $email = htmlspecialchars($_POST['email'] ?? '');
+    $phone = htmlspecialchars($_POST['phone'] ?? '');
+    $message = htmlspecialchars($_POST['message'] ?? '');
+
+    $body = "
+        <strong>Name:</strong> {$name}<br>
+        <strong>Email:</strong> {$email}<br>
+        <strong>Phone:</strong> {$phone}<br><br>
+        <strong>Message:</strong><br>{$message}
+    ";
+
+    $headers = "MIME-Version: 1.0" . "\r\n";
+    $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+    $headers .= "From: Website Contact <no-reply@sciencenewacademy.in>\r\n";
+
+    if (mail($to, $subject, $body, $headers)) {
+        http_response_code(200);
+        echo 'Mail sent';
+    } else {
+        http_response_code(500);
+        echo 'Mail failed';
+    }
+}
